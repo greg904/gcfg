f92fb7f9288670e4bcc29d75f759b7e598186d2b
Greg Depoire--Ferrer <greg@gregdf.com>
2022-06-05 10:10:14 +0200

    Build Linux kernel

--- gcfg-apply/src/main.rs
+++ gcfg-apply/src/main.rs
@@ -1,7 +1,7 @@
 #![feature(unix_chown)]
 
 use std::{
-    fs::File,
+    fs::{File, self},
     io::{self, Write},
     os::unix::prelude::{CommandExt, PermissionsExt},
     path::Path,
@@ -26,7 +26,7 @@ fn read_machine_from_hostname() -> io::Result<Machine> {
     }
 }
 
-fn clone_or_update_git_repo_as_user(origin: &str, path: impl AsRef<Path>) -> io::Result<()> {
+fn clone_or_update_git_repo_as_user(origin: &str, path: impl AsRef<Path>, only_clone: bool) -> io::Result<()> {
     // If the last pull was done recently, then we consider the
     // repository to be up-to-date. This makes the command faster
     // and prevents hammering the remote repository.
@@ -37,6 +37,9 @@ fn clone_or_update_git_repo_as_user(origin: &str, path: impl AsRef<Path>) -> io:
                     io::ErrorKind::Other,
                     "git repository destination is not a directory",
                 ));
+            }
+            if only_clone {
+                return Ok(());
             }
             let dot_git = path.as_ref().join(".git");
             let timeout = SystemTime::now() - Duration::from_secs(60 * 60 * 24);
@@ -95,6 +98,33 @@ fn clone_or_update_git_repo_as_user(origin: &str, path: impl AsRef<Path>) -> io:
             ),
         ));
     }
+    Ok(())
+}
+
+fn build_kernel_if_needed() -> io::Result<()> {
+    // Check if the kernel was already built.
+    match fs::metadata("/boot/EFI/Linux.efi") {
+        Ok(_) => return Ok(()),
+        Err(e) if e.kind() == io::ErrorKind::NotFound => {},
+        Err(e) => return Err(e),
+    }
+    let code = Command::new("make")
+        .arg("-j8")
+        .uid(1000)
+        .gid(1000)
+        .current_dir("/home/greg/linux")
+        .spawn()?
+        .wait()?;
+    if !code.success() {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!(
+                "Make returned with unsuccessful exit code in Linux kernel: {:?}",
+                code.code()
+            ),
+        ));
+    }
+    fs::copy("/home/greg/linux/arch/x86/boot/bzImage", "/boot/EFI/Linux.efi")?;
     Ok(())
 }
 
@@ -168,27 +198,39 @@ fn main() -> io::Result<()> {
         clone_or_update_git_repo_as_user(
             "https://github.com/lervag/vimtex.git",
             "/home/greg/.local/share/nvim/site/pack/my-plugins/start/vimtex",
+            false,
         )?;
         clone_or_update_git_repo_as_user(
             "https://github.com/neovim/nvim-lspconfig.git",
             "/home/greg/.local/share/nvim/site/pack/my-plugins/start/nvim-lspconfig",
+            false,
         )?;
         clone_or_update_git_repo_as_user(
             "https://github.com/quangnguyen30192/cmp-nvim-ultisnips.git",
             "/home/greg/.local/share/nvim/site/pack/my-plugins/start/cmp-nvim-ultisnips",
+            false,
         )?;
         clone_or_update_git_repo_as_user(
             "https://github.com/SirVer/ultisnips.git",
             "/home/greg/.local/share/nvim/site/pack/my-plugins/start/ultisnips",
+            false,
         )?;
         clone_or_update_git_repo_as_user(
             "https://github.com/hrsh7th/cmp-nvim-lsp.git",
             "/home/greg/.local/share/nvim/site/pack/my-plugins/start/cmp-nvim-lsp",
+            false,
         )?;
         clone_or_update_git_repo_as_user(
             "https://github.com/hrsh7th/nvim-cmp.git",
             "/home/greg/.local/share/nvim/site/pack/my-plugins/start/nvim-cmp",
+            false,
         )?;
+        clone_or_update_git_repo_as_user(
+            "/bubble/linux.git",
+            "/home/greg/linux",
+            true,
+        )?;
+        build_kernel_if_needed()?;
     }
     write_file(
         "/home/greg/.zshrc",
@@ -199,14 +241,17 @@ fn main() -> io::Result<()> {
     clone_or_update_git_repo_as_user(
         "https://github.com/zsh-users/zsh-autosuggestions.git",
         "/home/greg/.local/share/zsh-plugins/zsh-autosuggestions",
+        false,
     )?;
     clone_or_update_git_repo_as_user(
         "https://github.com/zsh-users/zsh-history-substring-search.git",
         "/home/greg/.local/share/zsh-plugins/zsh-history-substring-search",
+        false,
     )?;
     clone_or_update_git_repo_as_user(
         "https://github.com/editorconfig/editorconfig-vim.git",
         "/home/greg/.local/share/nvim/site/pack/my-plugins/start/editorconfig-vim",
+        false,
     )?;
     Ok(())
 }
